623dd5a289a6dd50f954cb856777761400bf9bb7
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-06-04 01:43:20 -0700

    Changes to be committed: 	modified:   components/Layout.js 	new file:   pages/cart.js 	new file:   pages/product/[productId].js

---
+++ pages/product/[productId].js
@@ -0,0 +1,14 @@
+import { useRouter, userRouter } from 'next/router';
+const ProductPage = () =>{
+  const router = useRouter()
+  const { productId } = router.query
+  return (
+    <div>
+      hi this is product page {productId}
+    </div>
+  )
+}
+
+export default ProductPage;
+
+'http://localhost:3000/product/1234512';
